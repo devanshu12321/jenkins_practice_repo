782d3b7729e6f3f9682cdd2c0aab5e725648315a
Devanshu Shrivastava <Devanshu Shrivastava@DESKTOP-57E8I4B>
2024-06-13 10:27:23 +0530

    fghj

---
+++ DemoJenkinsPractice/src/test/java/jenkins/Jenkins_Test.java
@@ -0,0 +1,15 @@
+ package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jenkins_Test 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("jenkins test is executed ");
+	}
+	
+
+}
